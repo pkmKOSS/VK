f15c3bd5fd225940f43e727c9f2c4bf783b79c5d
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2022-11-01 17:55:19 +0500

    VK - 1 экран

---
+++ VK/VK/AuthorizationScreen/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Главный экран.
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
