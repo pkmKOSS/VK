5a5f6c383e3c3c987ce24686606a66f720179117
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2022-12-06 22:13:40 +0500

    Lesson6-7 правки

---
+++ VK/VK/GroupNewsScreen/Cells/NameDateCell/NameDateCell.swift
@@ -0,0 +1,23 @@
+//
+//  NameDateCell.swift
+//  VK
+//
+//  Created by Григоренко Александр Игоревич on 06.12.2022.
+//
+
+import UIKit
+
+class NameDateCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ VK/VK/GroupNewsScreen/Cells/UsersReactionCell/UsersReactionsCell.swift
@@ -0,0 +1,23 @@
+//
+//  UsersReactionsCell.swift
+//  VK
+//
+//  Created by Григоренко Александр Игоревич on 06.12.2022.
+//
+
+import UIKit
+
+class UsersReactionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
